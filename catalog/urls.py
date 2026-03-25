ecf08f78d47dad8bbedc87e84f3410dc8f9c06c6
Chubarov Ilya <i-chubarov@mail.ru>
2026-03-25 21:03:56 +0300

    feat: add fixtures for Category and Product models

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,4 +6,5 @@ app_name = 'catalog'
 urlpatterns = [
     path('', views.home, name='home'),
     path('contacts/', views.contacts, name='contacts'),
+    path('product/<int:pk>/', views.product_detail, name='product_detail'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,24 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Product, Category
 
 
 def home(request):
     """Контроллер для домашней страницы"""
-    return render(request, 'catalog/home.html')
+    products = Product.objects.all()  # Получаем все продукты
+    return render(request, 'catalog/home.html', {'products': products})
 
 
 def contacts(request):
-    """Контроллер для страницы контактов с обработкой формы"""
+    """Контроллер для страницы контактов"""
     if request.method == 'POST':
-        # Получаем данные из формы
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
 
-        # Здесь можно добавить логику отправки email или сохранения в БД
         print(f'Получено сообщение от {name}: {message}, тел: {phone}')
-
-        # Можно вернуть сообщение об успехе
         return HttpResponse('Спасибо за ваше сообщение!')
 
     return render(request, 'catalog/contacts.html')
+
+
+def product_detail(request, pk):
+    """Контроллер для страницы одного товара"""
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'catalog/product_detail.html', {'product': product})

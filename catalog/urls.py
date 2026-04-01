9e49e73e24caf46a1d4ebc52087d617e9515abd8
Chubarov Ilya <i-chubarov@mail.ru>
2026-04-01 09:43:19 +0300

    feat: add CRUD for products with forms and validation

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('contacts/', views.ContactsView.as_view(), name='contacts'),
     path('product/<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
+    path('product/create/', views.ProductCreateView.as_view(), name='product_create'),
+    path('product/<int:pk>/update/', views.ProductUpdateView.as_view(), name='product_update'),
+    path('product/<int:pk>/delete/', views.ProductDeleteView.as_view(), name='product_delete'),
 ]
